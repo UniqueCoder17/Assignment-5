81228898ebcc50ca387a962985b485aed8dc74b1
UniqueCoder17 <raihanalamr08@gmail.com>
2024-09-23 20:04:41 +0600

    Donate Added

--- js/cashOut.js
+++ js/cashOut.js
@@ -23,4 +23,53 @@ document.getElementById('donate-btn')
     })
 
 
+document.getElementById('feni-donate-btn')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+
+        // if (isNaN(cashOut) || cashOut <= 0) {
+        //     alert('Please enter a valid amount to donate.');
+        //     return;
+        // }
+
+        const cashOut = getInputFieldValueById('feni-donate-input')
+        const balance = getTextFiledValueById('account-balance')
+
+        const newBalance = balance - cashOut;
+        document.getElementById('account-balance').innerText = newBalance;
+
+        const p = document.createElement('p');
+            p.innerText = `Added: ${addMoney} TK. New Balance: ${newBalance}`;
+            console.log(p);
+
+            document.getElementById('transaction-container').appendChild(p);
+
+
+    })
+
+document.getElementById('btn-quota')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+
+        // if (isNaN(cashOut) || cashOut <= 0) {
+        //     alert('Please enter a valid amount to donate.');
+        //     return;
+        // }
+
+        const cashOut = getInputFieldValueById('donate-quota-inpute')
+        const balance = getTextFiledValueById('account-balance')
+
+        const newBalance = balance - cashOut;
+        document.getElementById('account-balance').innerText = newBalance;
+
+        const p = document.createElement('p');
+            p.innerText = `Added: ${addMoney} TK. New Balance: ${newBalance}`;
+            console.log(p);
+
+            document.getElementById('transaction-container').appendChild(p);
+
+
+    })
+
+
 
--- js/donate.js
+++ js/donate.js
@@ -1,4 +1,4 @@
-
+const date = Date()
 
 document.getElementById('donate-btn')
     .addEventListener('click', function (event) {
@@ -14,6 +14,62 @@ document.getElementById('donate-btn')
 
         const newBalance = balance + addMoney;
         document.getElementById('donate-balance').innerText = newBalance;
+
+        const div = document.createElement('div');
+        div.classList.add('bg-yellow-300');
+        div.innerHTML = `
+            <h4 class="text-2xl font-bold">Cash Out</h4>
+            <p>${cashOut} Withdraw. New Balance ${newBalance}</p>
+        `
+
+        document.getElementById('transaction-container').appendChild(div);
+
+
+
+    })
+    // fenir 
+document.getElementById('feni-donate-btn')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+
+        // if(isNaN(cashOut)){
+        //     alert('Failed to cash out!');
+        //     return;
+        // }
+
+        const addMoney = getInputFieldValueById('feni-donate-input')
+        const balance = getTextFiledValueById('feni-donate-balance')
+
+        const newBalance = balance + addMoney;
+        document.getElementById('feni-donate-balance').innerText = newBalance;
+
+        const div = document.createElement('div');
+        div.classList.add('bg-yellow-300');
+        div.innerHTML = `
+            <h4 class="text-2xl font-bold">Cash Out</h4>
+            <p>${cashOut} Withdraw. New Balance ${newBalance}</p>
+        `
+
+        document.getElementById('transaction-container').appendChild(div);
+
+
+
+    })
+
+document.getElementById('btn-quota')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+
+        // if(isNaN(cashOut)){
+        //     alert('Failed to cash out!');
+        //     return;
+        // }
+
+        const addMoney = getInputFieldValueById('donate-quota-inpute')
+        const balance = getTextFiledValueById('donate-quota-balance')
+
+        const newBalance = balance + addMoney;
+        document.getElementById('donate-quota-balance').innerText = newBalance;
 
         const div = document.createElement('div');
         div.classList.add('bg-yellow-300');
